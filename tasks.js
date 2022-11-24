f96505327a4b5002a70b7ec6965e5a2bb33a1b97
Sergiu001 <101557082+Sergiu001@users.noreply.github.com>
2022-11-24 15:32:42 +0200

    Add files via upload
    first commit

---
+++ tasks.js
@@ -0,0 +1,10 @@
+var daily = [
+            "alt park",
+            "fractal key buy",
+            "eye of the north discounted supply box"
+            ];
+var weekly = [
+            "mystic clover buy",
+            "antique summoning stone buy",
+            "4 EOD Strikes"
+            ];
